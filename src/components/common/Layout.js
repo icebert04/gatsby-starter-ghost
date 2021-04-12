9c49a995f60b87e63fd00603a97bf756c0967ff1
Francis Isberto <icebert04@gmail.com>
2021-04-12 11:25:04 +0800

    &mdash;

--- src/components/common/Layout.js
+++ src/components/common/Layout.js
@@ -82,7 +82,7 @@ const DefaultLayout = ({ data, children, bodyClass, isHome }) => {
                     <footer className="site-foot">
                         <div className="site-foot-nav container">
                             <div className="site-foot-nav-left">
-                                <Link to="/">{site.title}</Link> mdash; all rights reserved 
+                                <Link to="/">{site.title}</Link> &mdash; all rights reserved 
                                 <a className="site-foot-nav-item" href="https://ghost.org" target="_blank" rel="noopener noreferrer"> Francis Isberto</a>
                                 </div>
                             <div className="site-foot-nav-right">
