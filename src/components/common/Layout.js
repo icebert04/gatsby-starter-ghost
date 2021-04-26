d9d940887d89a2d3df69be7093fee9eaa7ed633c
Francis Isberto <icebert04@gmail.com>
2021-04-26 14:58:57 +0800

    &copy;

--- src/components/common/Layout.js
+++ src/components/common/Layout.js
@@ -84,7 +84,7 @@ const DefaultLayout = ({ data, children, bodyClass, isHome }) => {
                     <footer className="site-foot">
                         <div className="site-foot-nav container">
                             <div className="site-foot-nav-left">
-                                <Link to="/">{site.title}</Link> &mdash; all rights reserved 2021
+                                <Link to="/">{site.title}</Link> &mdash; all rights reserved &copy; 2021
                             </div>
                             <div className="site-foot-nav-right">
                                 <Navigation data={site.navigation} navClass="site-foot-nav-item" />
